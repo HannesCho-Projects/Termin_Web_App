e0d68c70cdef5ffb929b461902a1b15da78bf741
HannesCho <hoonsungcho11@gmail.com>
2022-09-09 17:48:56 +0200

    add rawid and inlinemodel

--- houses/admin.py
+++ houses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -13,10 +14,18 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.houses.count()
 
 
+class Photoinline(admin.TabularInline):
+    model = models.Photo
+
+
 @admin.register(models.House)
 class HouseAdmin(admin.ModelAdmin):
 
     """House Admin Definition"""
+
+    inlines = [
+        Photoinline,
+    ]
 
     fieldsets = (
         (
@@ -63,6 +72,7 @@ class HouseAdmin(admin.ModelAdmin):
         "country",
     )
 
+    raw_id_fields = ("host",)
     search_fields = ("=city", "^host__username")
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
@@ -77,4 +87,11 @@ class HouseAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ """
+    """Photo Admin Definition"""
+
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f"<img width=50px src={obj.file.url} />")
+
+    get_thumbnail.short_description = "Thumbnail"
