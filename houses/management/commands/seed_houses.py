2e09c4fe4766ce24beef0ba0190e421e52f8255f
HannesCho <hoonsungcho11@gmail.com>
2022-09-12 17:48:58 +0200

    seed houses fix errors

--- houses/management/commands/seed_houses.py
+++ houses/management/commands/seed_houses.py
@@ -1,5 +1,6 @@
 import random
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from houses import models as house_models
 from users import models as user_models
@@ -18,14 +19,14 @@ class Command(BaseCommand):
         number = options.get("number")
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
-        house_types = house_models.RoomType.objects.all()
+        house_type = house_models.HouseType.objects.all()
         seeder.add_entity(
-            house_models.Room,
+            house_models.House,
             number,
             {
                 "name": lambda x: seeder.faker.address(),
                 "host": lambda x: random.choice(all_users),
-                "house_types": lambda x: random.choice(house_types),
+                "house_type": lambda x: random.choice(house_type),
                 "guests": lambda x: random.randint(1, 20),
                 "price": lambda x: random.randint(1, 300),
                 "beds": lambda x: random.randint(1, 5),
@@ -33,5 +34,30 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        amenities = house_models.Amenity.objects.all()
+        facilities = house_models.Facility.objects.all()
+        rules = house_models.HouseRule.objects.all()
+        for pk in created_clean:
+            house = house_models.Room.objects.get(pk=pk)
+            for i in range(1, random.randint(3, 5)):
+                house_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    house=house,
+                    file=f"house_photos/{random.randint(1, 31)}.webp",
+                )
+            for a in amenities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    house.amenities.add(a)
+            for f in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    house.facilities.add(f)
+            for r in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    house.house_rules.add(r)
+
         self.stdout.write(self.style.SUCCESS(f"{number} houses created!"))
