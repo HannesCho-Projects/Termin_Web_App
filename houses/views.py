532d9d97c6a20e05d41a3e65cca525855ee88409
HannesCho <hoonsungcho11@gmail.com>
2022-09-16 20:58:59 +0200

    pagination1

--- houses/views.py
+++ houses/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from http.client import HTTPResponse
 from datetime import datetime
 from django.shortcuts import render
@@ -5,5 +6,20 @@ from . import models
 
 
 def all_houses(request):
-    all_houses = models.House.objects.all()
-    return render(request, "home.html", context={"houses": all_houses})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_houses = models.House.objects.all()[offset:limit]
+    page_count = ceil(models.House.objects.count() / page_size)
+    return render(
+        request,
+        "home.html",
+        context={
+            "houses": all_houses,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
+    )
