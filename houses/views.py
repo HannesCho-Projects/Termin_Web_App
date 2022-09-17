7e144af3bb0fe074c07d4c2b8ff068845bad3dba
HannesCho <hoonsungcho11@gmail.com>
2022-09-17 22:21:27 +0200

    Django paginator

--- houses/views.py
+++ houses/views.py
@@ -1,25 +1,29 @@
 from math import ceil
-from http.client import HTTPResponse
-from datetime import datetime
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from . import models
 
 
 def all_houses(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_houses = models.House.objects.all()[offset:limit]
-    page_count = ceil(models.House.objects.count() / page_size)
-    return render(
-        request,
-        "home.html",
-        context={
-            "houses": all_houses,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    # page = request.GET.get("page", 1)
+    # page = int(page or 1)
+    # page_size = 10
+    # limit = page_size * page
+    # offset = limit - page_size
+    # all_houses = models.House.objects.all()[offset:limit]
+    # page_count = ceil(models.House.objects.count() / page_size)
+    # return render(
+    #     request,
+    #     "home.html",
+    #     context={
+    #         "houses": all_houses,
+    #         "page": page,
+    #         "page_count": page_count,
+    #         "page_range": range(1, page_count),
+    #     },
+    # )
+    page = request.GET.get("page")
+    house_list = models.House.objects.all()
+    paginator = Paginator(house_list, 10)
+    houses = paginator.get_page(page)
+    return render(request, "home.html", {"houses": houses})
