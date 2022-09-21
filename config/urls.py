1193d0cbfe8f2c4de2840b47cfe68ee9efb08ee6
HannesCho <hoonsungcho11@gmail.com>
2022-09-21 12:04:51 +0200

    Login setup

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("houses/", include("houses.urls", namespace="houses")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
