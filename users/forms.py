d8a1ca9fbce6cef23e1f0d5ee0e38ce65a1a2a90
HannesCho <hoonsungcho11@gmail.com>
2022-09-22 16:43:47 +0200

    signup view

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,28 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    passwordc = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            email = models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exsist with that email")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_passwordc(self):
+        password = self.cleaned_data.get("password")
+        passwordc = self.cleaned_data.get("passwordc")
+        if password != passwordc:
+            raise forms.ValidationError("Password Confirmation does not match")
+        else:
+            return password
